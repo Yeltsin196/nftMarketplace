f03cbbb51027fdbee0f0756afe58ba65348d6ef5
Yeltsin <yramirez@shokworks.io>
2022-09-29 12:18:44 -0400

    wallet lock out

--- components/providers/web3/index.tsx
+++ components/providers/web3/index.tsx
@@ -10,9 +10,25 @@ import { MetaMaskInpageProvider } from "@metamask/providers";
 
 /* import { Contract, providers } from "ethers"; */
 
-function pageReload() {
+const pageReload = () => {
   window.location.reload();
-}
+};
+const handleAccount = (ethereum: MetaMaskInpageProvider) => async () => {
+  const isLocked = !(await ethereum._metamask.isUnlocked());
+  if (isLocked) {
+    pageReload();
+  }
+};
+
+const setGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
+  ethereum.on("chainChanged", pageReload);
+  ethereum.on("accountsChanged", handleAccount(ethereum));
+};
+
+const removeGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
+  ethereum.removeListener("chainChanged", pageReload);
+  ethereum.removeListener("accountsChanged", handleAccount);
+};
 import {
   createDefaultState,
   createWeb3State,
@@ -61,14 +77,6 @@ const Web3Provider: FunctionComponent<web3ContextProps> = ({ children }) => {
     initWeb3();
     return () => removeGlobalListeners(window.ethereum);
   }, []);
-
-  const setGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
-    ethereum.on("chainChanged", pageReload);
-  };
-
-  const removeGlobalListeners = (ethereum: MetaMaskInpageProvider) => {
-    ethereum.removeListener("chainChanged", pageReload);
-  };
 
   return (
     <Web3Context.Provider value={web3Api}>{children}</Web3Context.Provider>
