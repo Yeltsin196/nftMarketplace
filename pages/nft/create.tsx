488eb563cce881e2e087ffb8368cf614a7016d6e
Yeltsin <yramirez@shokworks.io>
2022-10-11 16:23:26 -0400

    Store Image to State

--- pages/nft/create.tsx
+++ pages/nft/create.tsx
@@ -5,7 +5,7 @@ import { ChangeEvent, useState } from "react";
 import { BaseLayout } from "@ui";
 import { Switch } from "@headlessui/react";
 import Link from "next/link";
-import { NftMeta } from "@_types/nft";
+import { NftMeta, PinataRes } from "@_types/nft";
 import axios from "axios";
 import { useWeb3 } from "@providers/web3";
 
@@ -23,6 +23,56 @@ const NftCreate: NextPage = () => {
       { trait_type: "speed", value: "0" },
     ],
   });
+
+  const getSignedData = async () => {
+    const messageToSign = await axios.get("/api/verify");
+    const accounts = (await ethereum?.request({
+      method: "eth_requestAccounts",
+    })) as string[];
+    const account = accounts[0];
+
+    const signedData = await ethereum?.request({
+      method: "personal_sign",
+      params: [
+        JSON.stringify(messageToSign.data),
+        account,
+        messageToSign.data.id,
+      ],
+    });
+
+    return { signedData, account };
+  };
+
+  const handleImage = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      console.error("Select a file");
+      return;
+    }
+
+    const file = e.target.files[0];
+    const buffer = await file.arrayBuffer();
+    const bytes = new Uint8Array(buffer);
+
+    try {
+      const { signedData, account } = await getSignedData();
+      const res = await axios.post("/api/verify-image", {
+        address: account,
+        signature: signedData,
+        bytes,
+        contentType: file.type,
+        fileName: file.name.replace(/\.[^/.]+$/, ""),
+      });
+
+      const data = res.data as PinataRes;
+
+      setNftMeta({
+        ...nftMeta,
+        image: `${process.env.NEXT_PUBLIC_PINATA_DOMAIN}/ipfs/${data.IpfsHash}`,
+      });
+    } catch (e: any) {
+      console.error(e.message);
+    }
+  };
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -46,20 +96,7 @@ const NftCreate: NextPage = () => {
 
   const createNft = async () => {
     try {
-      const messageToSign = await axios.get("/api/verify");
-      const accounts = (await ethereum?.request({
-        method: "eth_requestAccounts",
-      })) as string[];
-      const account = accounts[0];
-
-      const signedData = await ethereum?.request({
-        method: "personal_sign",
-        params: [
-          JSON.stringify(messageToSign.data),
-          account,
-          messageToSign.data.id,
-        ],
-      });
+      const { signedData, account } = await getSignedData();
 
       await axios.post("/api/verify", {
         address: account,
@@ -234,12 +271,8 @@ const NftCreate: NextPage = () => {
                       </p>
                     </div>
                     {/* Has Image? */}
-                    {false ? (
-                      <img
-                        src="https://eincode.mypinata.cloud/ipfs/QmaQYCrX9Fg2kGijqapTYgpMXV7QPPzMwGrSRfV9TvTsfM/Creature_1.png"
-                        alt=""
-                        className="h-40"
-                      />
+                    {nftMeta.image ? (
+                      <img src={nftMeta.image} alt="" className="h-40" />
                     ) : (
                       <div>
                         <label className="block text-sm font-medium text-gray-700">
@@ -268,6 +301,7 @@ const NftCreate: NextPage = () => {
                               >
                                 <span>Upload a file</span>
                                 <input
+                                  onChange={handleImage}
                                   id="file-upload"
                                   name="file-upload"
                                   type="file"
